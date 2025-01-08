f9337886e7abb667eee8cbbaa229537c884cc1b3
Charlie Franzel <charliefranzel@gmail.com>
2025-01-08 10:58:32 -0700

    dasfdsafa

--- calculator.py
+++ calculator.py
@@ -1,2 +1,10 @@
 def binary_calculator(bin1, bin2, operator):
-    # your code here
+    bin1l=[char for char in bin1]
+    bin1l.reverse()
+    print(int(bin1l[0])^0)
+
+
+
+
+
+binary_calculator("00000002","00000001","+")
